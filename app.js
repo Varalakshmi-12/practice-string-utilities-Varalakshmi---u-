c2f1cd8d247463ceec2147c55a912fc352a2e61c
Varalakshmi12 <pandu.chetan27@gmail.com>
2025-09-06 19:44:13 -0500

    modified app.js file

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const utils=require('./stringUtils.js');
+
+//test capitalize
 console.log(utils.capitalize("hello"));
 console.log(utils.capitalize(""));
 console.log(utils.capitalize(undefined));
+//test reverse
 console.log(utils.reverse("hello"));
 console.log(utils.reverse("123"));
 console.log(utils.reverse(undefined));
+//test contains
 console.log(utils.contains("hello world","wr"));
 console.log(utils.contains(undefined,'wr'));
 console.log(utils.contains("hello world","wo"));
